1a2230b56bc058234951d8b9589c7e3323aec997
Max <101079236+MaxwellWellman@users.noreply.github.com>
2024-11-23 00:22:46 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,14 +1,34 @@
-// @name My Script
-// @description This script even does the laundry!
-// @copyright Year, Author (Author Website)
-// @license License Type; License Homepage
-// @icon url
-// @homepageURL url
-// @supportURL url
-// @contributionURL url
-// @version 1.0.0
-// @updateURL https://openuserjs.org/meta/maxwellwellman/My_Script.meta.js
-// @author maxwellwellman
+// ==UserScript==
+// @name Test
+// @namespace https://userscripts.org/scripts/show/155840
+// @description Description
+// @author MaxwellWellman
+// @version 10.0
+// @encoding utf-8
+// @license https://creativecommons.org/licenses/by-sa/4.0/
+// @icon
+// @homepage https://github.com/MaxwellWellman/purge-mod-test
+// @updateURL https://raw.githubusercontent.com/MaxwellWellman/purge-mod-test/refs/heads/master/mod.js
+// @downloadURL https://raw.githubusercontent.com/MaxwellWellman/purge-mod-test/refs/heads/master/mod.js
+// @include https://maxwell-wellman.itch.io/purge-demo
+// @grant unsafeWindow
+// @grant GM_addStyle
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// @grant GM_registerMenuCommand
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_log
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant GM_getMetadata
+// @run-at document-start
+// @connect *
+// ==/UserScript==
 
 (async function() {
     
