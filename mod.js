16b4362c0d89d41b27eebbc586371080e208ee7f
Max <101079236+MaxwellWellman@users.noreply.github.com>
2024-11-23 00:00:16 +0300

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,28 @@
+/*
+    @name Test
+    @description Mod for PURGE
+    @match https://maxwell-wellman.itch.io/purge-demo/*
+    @connect github.com
+    @run-at document-start
+    @downloadURL lol.ru
+    @grant GM_setValue
+    @grant GM_deleteValue
+    @grant GM_listValues
+    @grant GM_addValueChangeListener
+    @grant GM_openInTab
+    @grant GM_xmlhttpRequest
+    @grant GM.getValue
+    @grant GM.listValues
+    @grant GM.openInTab
+    @grant GM.xmlHttpRequest
+    @icon data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
+
+*/
+
+
+(async function() {
+    
+    'use strict'
+    console.log('hello')
+    
+})()
