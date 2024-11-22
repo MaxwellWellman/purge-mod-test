bc31b4415c8fde03108aa7a062a629609e4a3308
Max <101079236+MaxwellWellman@users.noreply.github.com>
2024-11-23 00:10:52 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,24 +1,26 @@
-/*
-    @name Test
-    @description Mod for PURGE
-    @match https://maxwell-wellman.itch.io/purge-demo/*
-    @connect github.com
-    @run-at document-start
-    @downloadURL https://raw.githubusercontent.com/MaxwellWellman/purge-mod-test/refs/heads/master/mod.js
-    @grant GM_setValue
-    @grant GM_deleteValue
-    @grant GM_listValues
-    @grant GM_addValueChangeListener
-    @grant GM_openInTab
-    @grant GM_xmlhttpRequest
-    @grant GM.getValue
-    @grant GM.listValues
-    @grant GM.openInTab
-    @grant GM.xmlHttpRequest
-    @icon data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
+// ==UserScript==
+// @name Test
+// @description Mod for PURGE
+// @match https://maxwell-wellman.itch.io/purge-demo/*
+// @connect github.com
+// @run-at document-start
+// @downloadURL https://raw.githubusercontent.com/MaxwellWellman/purge-mod-test/refs/heads/master/mod.js
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_addValueChangeListener
+// @grant GM_openInTab
+// @grant GM_xmlhttpRequest
+// @grant GM.getValue
+// @grant GM.listValues
+// @grant GM.openInTab
+// @grant GM.xmlHttpRequest
+// @icon data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
+// ==UserScript==
 
-*/
-
+/**
+ * Would you shut the fuck up.
+ */
 
 (async function() {
     
